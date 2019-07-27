6bfe770d2cdef5462cc1103b29f32f9df4a2d57e
tangweihua163 <tangweihua163@gmail.com>
2019-07-28 04:25:41 +0800

    0027.RemoveElement

--- 0027.RemoveElement_test.go
+++ 0027.RemoveElement_test.go
@@ -1,14 +1,1 @@
 package leetcode_go
-
-func removeElement(nums []int, val int) int {
-	index := 0
-
-	for i := 0; i < len(nums); i++ {
-		if nums[i] != val {
-			nums[index] = nums[i]
-			index++
-		}
-	}
-
-	return index
-}
