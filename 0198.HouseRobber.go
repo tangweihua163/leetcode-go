9e093a5922dff118f6c2e77e2c6ec77d8a8f7005
tangweihua163 <tangweihua163@gmail.com>
2019-07-29 07:42:53 +0800

    0198.HouseRobber

--- 0198.HouseRobber.go
+++ 0198.HouseRobber.go
@@ -1,1 +1,37 @@
 package leetcode_go
+
+func rob(nums []int) int {
+	if len(nums) == 0 {
+		return 0
+	}
+
+	if len(nums) == 1 {
+		return nums[0]
+	}
+
+	if len(nums) == 2 {
+		return max(nums[0], nums[1])
+	}
+
+	nums[1] = max(nums[0], nums[1])
+	for i := 2; i < len(nums); i++ {
+		nums[i] = max(nums[i-1], nums[i-2]+nums[i])
+	}
+
+	max := 0
+	for _, v := range nums {
+		if v > max {
+			max = v
+		}
+	}
+
+	return max
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	} else {
+		return b
+	}
+}
