fdf2cc53d5715cd58c4c67503e01cc4b2cb01b66
tangweihua163 <tangweihua163@gmail.com>
2019-07-29 19:28:55 +0800

    0003.LongestSubstringWithoutRepeatingCharacters

--- 0003.LongestSubstringWithoutRepeatingCharacters.go
+++ 0003.LongestSubstringWithoutRepeatingCharacters.go
@@ -33,3 +33,37 @@ func lengthOfLongestSubstring(s string) int {
 	return maxlength
 
 }
+
+func lengthOfLongestSubstring2(s string) int {
+	if len(s) == 0 {
+		return 0
+	}
+
+	var start, end int
+	var maxlength = end - start + 1
+
+	var bytes [256]byte
+
+	bytes[s[start]] = 1
+
+	for i := 1; i < len(s); i++ {
+		if bytes[s[i]] == 0 {
+			end = i
+			bytes[s[end]] = 1
+			if maxlength < end-start+1 {
+				maxlength = end - start + 1
+			}
+			continue
+		}
+
+		for s[start] != s[i] {
+			bytes[s[start]] = 0
+			start++
+		}
+
+		start++
+
+	}
+
+	return maxlength
+}
