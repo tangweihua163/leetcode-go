bffcfa1d82b42428d023dcac683a3eb7cd57ef8e
tangweihua163 <tangweihua163@gmail.com>
2019-07-30 20:33:29 +0800

    0000.Common

---
+++ 0000.Common.go
@@ -0,0 +1,111 @@
+package leetcode_go
+
+func MinInt(arr ...int) (val int, index int) {
+	min := arr[0]
+	minIndex := 0
+	for i, v := range arr {
+		if v < min {
+			min = v
+			minIndex = i
+		}
+	}
+	return min, minIndex
+}
+
+func MaxInt(arr ...int) (val int, index int) {
+	max := arr[0]
+	maxIndex := 0
+	for i, v := range arr {
+		if v > max {
+			max = v
+			maxIndex = i
+		}
+	}
+	return max, maxIndex
+}
+
+// 堆排序（升序）
+func HeapSortASC(A []int) {
+	BuildMaxHeap(A)
+	for i := len(A) - 1; i > 0; i-- {
+		A[i], A[0] = A[0], A[i]
+		A = A[:i]
+		AdjustDownMaxHeap(A, 0)
+	}
+}
+
+// 构建最大堆
+func BuildMaxHeap(A []int) {
+	start := (len(A) - 1) / 2
+	for i := start; i >= 0; i-- {
+		AdjustDownMaxHeap(A, i)
+	}
+}
+
+// 调整最大堆节点
+func AdjustDownMaxHeap(A []int, i int) {
+	if i > len(A)/2-1 {
+		return
+	}
+
+	l := len(A)
+	left := i*2 + 1
+	right := i*2 + 2
+
+	if right < l {
+		if A[i] < A[right] && A[left] <= A[right] {
+			A[i], A[right] = A[right], A[i]
+			AdjustDownMaxHeap(A, right)
+		} else if A[i] < A[left] && A[right] <= A[left] {
+			A[i], A[left] = A[left], A[i]
+			AdjustDownMaxHeap(A, left)
+		}
+	} else {
+		if A[i] < A[left] {
+			A[i], A[left] = A[left], A[i]
+		}
+	}
+}
+
+// 堆排序（降序）
+func HeapSortDESC(A []int) {
+	BuildMinHeap(A)
+	for i := len(A) - 1; i > 0; i-- {
+		A[i], A[0] = A[0], A[i]
+		A = A[:i]
+		AdjustDownMinHeap(A, 0)
+	}
+}
+
+// 构建最小堆
+func BuildMinHeap(A []int) {
+	start := (len(A) - 1) / 2
+	for i := start; i >= 0; i-- {
+		AdjustDownMinHeap(A, i)
+	}
+}
+
+// 调整最小堆节点
+func AdjustDownMinHeap(A []int, i int) {
+	if i > len(A)/2-1 {
+		return
+	}
+
+	l := len(A)
+	left := i*2 + 1
+	right := i*2 + 2
+
+	if right < l {
+		if A[i] > A[right] && A[left] >= A[right] {
+			A[i], A[right] = A[right], A[i]
+			AdjustDownMinHeap(A, right)
+		} else if A[i] > A[left] && A[right] >= A[left] {
+			A[i], A[left] = A[left], A[i]
+			AdjustDownMinHeap(A, left)
+		}
+	} else {
+		if A[i] > A[left] {
+			A[i], A[left] = A[left], A[i]
+		}
+	}
+}
---
+++ 0000.Common_test.go
@@ -0,0 +1,26 @@
+package leetcode_go
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestMinInt(t *testing.T) {
+	fmt.Println(MinInt([]int{1, 2, 3, 4, 5}...))
+}
+
+func TestMaxInt(t *testing.T) {
+	fmt.Println(MaxInt([]int{197, 37192, 48, 349, 712}...))
+}
+
+func TestHeapSortASC(t *testing.T) {
+	A := []int{25, 346, 65, 52, 23, 5, 452, 23, 434224}
+	HeapSortASC(A)
+	fmt.Println(A)
+}
+
+func TestHeapSortDESC(t *testing.T) {
+	A := []int{25, 346, 65, 52, 23, 5, 452, 23, 434224}
+	HeapSortDESC(A)
+	fmt.Println(A)
+}
