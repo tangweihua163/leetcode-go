14dad575d50ef3cbba2513fe6cf46d0a5686e6ee
tangweihua163 <tangweihua163@gmail.com>
2019-07-31 17:28:39 +0800

    0037.SudokuSolver

--- 0037.SudokuSolver.go
+++ 0037.SudokuSolver.go
@@ -1,77 +1,65 @@
 package leetcode_go
 
-var row = make([]map[byte]int, 9)
-var col = make([]map[byte]int, 9)
-var square = make([]map[byte]int, 9)
-
-var candidate []int
-
-var suc = false
-
 func solveSudoku(board [][]byte) {
 
-	suc = false
-	candidate = make([]int, 0, 162)
+	var candidate = make([]int, 0, 162)
 
-	for i := 0; i < len(board); i++ {
-		row[i] = make(map[byte]int)
-		col[i] = make(map[byte]int)
-		square[i] = make(map[byte]int)
-	}
+	var row = make([]byte, 81)
+	var col = make([]byte, 81)
+	var square = make([]byte, 81)
 
-	for i := 0; i < len(board); i++ {
-		for j := 0; j < len(board); j++ {
-			if board[i][j] == '.' {
+	for i := 0; i < 9; i++ {
+		for j := 0; j < 9; j++ {
+			b := board[i][j]
+			if b == '.' {
 				candidate = append(candidate, i, j)
-				row[i][board[i][j]] = 0
-				col[j][board[i][j]] = 0
-				square[i/3*3+j/3][board[i][j]] = 0
 				continue
 			}
-			row[i][board[i][j]] = 1
-			col[j][board[i][j]] = 1
-			square[i/3*3+j/3][board[i][j]] = 1
+			row[i*9+int(b-'1')] = 1
+			col[j*9+int(b-'1')] = 1
+			square[(i/3*3+j/3)*9+int(b-'1')] = 1
 		}
 	}
 
 	cur := len(candidate) - 1
 
-	solve(board, cur)
+	solve(board, cur, candidate, row, col, square)
 
 }
 
-func solve(board [][]byte, cur int) {
+func solve(board [][]byte, cur int, candidate []int, row, col, square []byte) bool {
 
 	if cur < 0 {
-		suc = true
-		return
+		return true
 	}
 
-	var num byte
-	for num = '1'; num <= '9'; num++ {
+	var b byte
+	for b = '1'; b <= '9'; b++ {
 		var i = candidate[cur-1]
 		var j = candidate[cur]
-		r := row[i]
-		c := col[j]
-		s := square[i/3*3+j/3]
-		if r[num] == 0 && c[num] == 0 && s[num] == 0 {
+
+		r := row[i*9 : i*9+9]
+		c := col[j*9 : j*9+9]
+		s := square[(i/3*3+j/3)*9 : (i/3*3+j/3)*9+9]
+
+		if r[b-'1'] == 0 && c[b-'1'] == 0 && s[b-'1'] == 0 {
 			cur -= 2
-			r[num] = 1
-			c[num] = 1
-			s[num] = 1
-			board[i][j] = num
+			r[b-'1'] = 1
+			c[b-'1'] = 1
+			s[b-'1'] = 1
+			board[i][j] = b
 
-			solve(board, cur)
-
-			if suc {
-				return
+			if solve(board, cur, candidate, row, col, square) {
+				return true
 			}
 
 			board[i][j] = '.'
-			r[num] = 0
-			c[num] = 0
-			s[num] = 0
+			r[b-'1'] = 0
+			c[b-'1'] = 0
+			s[b-'1'] = 0
 			cur += 2
 		}
 	}
+
+	return false
 }
