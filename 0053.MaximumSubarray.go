72af2b0ac0d0060ecd029f17f73d43e01290aa84
tangweihua163 <tangweihua163@gmail.com>
2019-08-01 00:49:05 +0800

    0053.MaximumSubarray

--- 0053.MaximumSubarray.go
+++ 0053.MaximumSubarray.go
@@ -1,32 +1,17 @@
 package leetcode_go
 
-import "math"
-
 func maxSubArray(nums []int) int {
-
 	a := nums[0]
-	b := math.MinInt64
-
-	maxSum := max(a, b)
-
+	b := a
 	for i := 1; i < len(nums); i++ {
-		b = max(a, b)
-
-		if b > maxSum {
-			maxSum = b
-		}
-
 		if a > 0 {
 			a += nums[i]
 		} else {
 			a = nums[i]
 		}
-
-		if a > maxSum {
-			maxSum = a
+		if a > b {
+			b = a
 		}
-
 	}
-
-	return maxSum
+	return b
 }
