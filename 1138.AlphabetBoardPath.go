3410a1c356a1e6f41908a4b27130521cfaa8204a
tangweihua163 <tangweihua163@gmail.com>
2019-08-03 01:25:25 +0800

    1138.AlphabetBoardPath

--- 1138.AlphabetBoardPath.go
+++ 1138.AlphabetBoardPath.go
@@ -1,126 +1,67 @@
 package leetcode_go
 
-func alphabetBoardPath(target string) string {
-	var result []byte
+var result []byte
 
-	row, col := 0, 0
+var row, col, r, c, dr, dc int
+
+func alphabetBoardPath(target string) string {
+	result = []byte{}
+
+	row, col = 0, 0
 
 	for i := 0; i < len(target); i++ {
 		index := int(target[i] - 'a')
 
-		r := index / 5
-		c := index % 5
+		r = index / 5
+		c = index % 5
 
-		dr := r - row
-		dc := c - col
-
-		if row == 5 {
-
-			if dr > 0 {
-				for dr != 0 {
-					result = append(result, 'D')
-					dr--
-				}
-			}
-			if dr < 0 {
-				for dr != 0 {
-					result = append(result, 'U')
-					dr++
-				}
-			}
-
-			if dc > 0 {
-				for dc != 0 {
-					result = append(result, 'R')
-					dc--
-				}
-			}
-			if dc < 0 {
-				for dc != 0 {
-					result = append(result, 'L')
-					dc++
-				}
-			}
-
-			result = append(result, '!')
-
-			row = r
-			col = c
-
-			continue
-
-		}
+		dr = r - row
+		dc = c - col
 
 		if r == 5 {
-			if dc > 0 {
-				for dc != 0 {
-					result = append(result, 'R')
-					dc--
-				}
-			}
-			if dc < 0 {
-				for dc != 0 {
-					result = append(result, 'L')
-					dc++
-				}
-			}
-
-			if dr > 0 {
-				for dr != 0 {
-					result = append(result, 'D')
-					dr--
-				}
-			}
-			if dr < 0 {
-				for dr != 0 {
-					result = append(result, 'U')
-					dr++
-				}
-			}
-
+			adjustCol()
+			adjustRow()
 			result = append(result, '!')
-
 			row = r
 			col = c
-
-			continue
-
+		} else {
+			adjustRow()
+			adjustCol()
+			result = append(result, '!')
+			row = r
+			col = c
 		}
-
-		if dr > 0 {
-			for dr != 0 {
-				result = append(result, 'D')
-				dr--
-			}
-		}
-		if dr < 0 {
-			for dr != 0 {
-				result = append(result, 'U')
-				dr++
-			}
-		}
-
-		if dc > 0 {
-			for dc != 0 {
-				result = append(result, 'R')
-				dc--
-			}
-		}
-		if dc < 0 {
-			for dc != 0 {
-				result = append(result, 'L')
-				dc++
-			}
-		}
-
-		result = append(result, '!')
-
-		row = r
-		col = c
-
-		continue
-
 	}
 
 	return string(result)
+}
+
+func adjustRow() {
+	if dr > 0 {
+		for dr != 0 {
+			result = append(result, 'D')
+			dr--
+		}
+	}
+	if dr < 0 {
+		for dr != 0 {
+			result = append(result, 'U')
+			dr++
+		}
+	}
+}
+
+func adjustCol() {
+	if dc > 0 {
+		for dc != 0 {
+			result = append(result, 'R')
+			dc--
+		}
+	}
+	if dc < 0 {
+		for dc != 0 {
+			result = append(result, 'L')
+			dc++
+		}
+	}
 }
