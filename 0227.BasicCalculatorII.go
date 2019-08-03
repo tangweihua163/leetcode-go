e4b476dc4375054214b807bb9075565af7b611c6
tangweihua163 <tangweihua163@gmail.com>
2019-08-03 19:35:02 +0800

    0227.BasicCalculatorII

---
+++ 0227.BasicCalculatorII.go
@@ -0,0 +1,61 @@
+package leetcode_go
+
+func calculate(s string) int {
+
+	number := make([]int, 0, len(s))
+	operator := make([]byte, 0, len(s))
+
+	number = append(number, 0)
+	operator = append(operator, '+')
+
+	var n = 0
+	for i := 0; i < len(s); i++ {
+		switch s[i] {
+		case '1', '2', '3', '4', '5', '6', '7', '8', '9', '0':
+			n = n*10 + int(s[i]-'0')
+		case '+', '-', '*', '/':
+			ln := len(number)
+			lo := len(operator)
+			top := operator[lo-1]
+			if top == '*' {
+				number[ln-1] = number[ln-1] * n
+				operator[lo-1] = s[i]
+			} else if top == '/' {
+				number[ln-1] = number[ln-1] / n
+				operator[lo-1] = s[i]
+			} else {
+				number = append(number, n)
+				operator = append(operator, s[i])
+			}
+			n = 0
+		}
+	}
+	number = append(number, n)
+
+	ln := len(number)
+	lo := len(operator)
+	top := operator[lo-1]
+	if top == '*' {
+		number[ln-2] = number[ln-2] * number[ln-1]
+		number = number[:ln-1]
+		operator = operator[:lo-1]
+	} else if top == '/' {
+		number[ln-2] = number[ln-2] / number[ln-1]
+		number = number[:ln-1]
+		operator = operator[:lo-1]
+	}
+
+	i := 0
+	j := 0
+	for i < len(number)-1 && j < len(operator) {
+		if operator[j] == '+' {
+			number[i+1] = number[i] + number[i+1]
+		} else if operator[j] == '-' {
+			number[i+1] = number[i] - number[i+1]
+		}
+		i++
+		j++
+	}
+
+	return number[len(number)-1]
+}
---
+++ 0227.BasicCalculatorII_test.go
@@ -0,0 +1,14 @@
+package leetcode_go
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestCalculate(t *testing.T) {
+	fmt.Println(calculate("3+2*2+3"))
+	fmt.Println(calculate("3/2"))
+	fmt.Println(calculate("3+5 / 2"))
+	fmt.Println(calculate("3+22*2"))
+	fmt.Println(calculate("3+2*2-2"))
+}
