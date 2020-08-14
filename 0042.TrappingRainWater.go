116a31c6fe4b776e88100515a65e35460784fb2f
Weihua Tang <tangweihua163@gmail.com>
2020-08-14 18:30:05 +0800

    0042.TrappingRainWater

--- 0042.TrappingRainWater.go
+++ 0042.TrappingRainWater.go
@@ -50,3 +50,56 @@ func trap(height []int) int {
 
 	return sum
 }
+
+func trap2(height []int) int {
+
+	// 1 至少三个才能装到水
+	if len(height) < 3 {
+		return 0
+	}
+
+	// 2 统计黑色总面积、最高高度
+	var block = 0
+	var maxHeight = 0
+	for _, h := range height {
+		if h > maxHeight {
+			maxHeight = h
+		}
+		block += h
+	}
+
+	// 3 统计装满水后空白部分的高度
+	var l = 0
+	var lh = 0
+	var r = len(height) - 1
+	var rh = 0
+	var space = 0
+
+	// 3.1 从左边扫描空白
+	for l < r && height[l] < maxHeight {
+
+		if height[l] > lh {
+			lh = height[l]
+		}
+
+		space = space + (maxHeight - lh)
+
+		l++
+	}
+
+	// 3.2 从右边扫描空白
+	for l < r && height[r] < maxHeight {
+
+		if height[r] > rh {
+			rh = height[r]
+		}
+
+		space = space + (maxHeight - rh)
+
+		r--
+	}
+
+	// 4 总面积 - 黑色面积 - 空白面积 = 水的面积
+	return maxHeight*len(height) - space - block
+
+}
--- 0042.TrappingRainWater_test.go
+++ 0042.TrappingRainWater_test.go
@@ -9,4 +9,8 @@ func TestTrap(t *testing.T) {
 	fmt.Println(trap([]int{0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1}))
 	//
 	fmt.Println(trap([]int{2, 1, 0, 2}))
+
+	fmt.Println(trap2([]int{0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1}))
+	//
+	fmt.Println(trap2([]int{2, 1, 0, 2}))
 }
