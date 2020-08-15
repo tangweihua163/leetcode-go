102fcf3716e5706490d7377b3fcf48cbe6610a5c
Weihua Tang <tangweihua163@gmail.com>
2020-08-16 05:40:17 +0800

    optimize

--- 0033.SearchInRotatedSortedArray.go
+++ 0033.SearchInRotatedSortedArray.go
@@ -1,74 +1,47 @@
 package leetcode_go
 
 func searchInRotatedSortedArray(nums []int, target int) int {
-	return searchInRange(nums, 0, len(nums)-1, target)
-}
 
-func searchInRange(nums []int, left, right int, target int) int {
-
-	if right-left <= 5 {
-		return linearSearch(nums, left, right, target)
+	if len(nums) == 0 {
+		return -1
 	}
 
-	if nums[left] < nums[right] {
-		return binarySearch(nums, left, right, target)
-	}
-
+	var left, right = 0, len(nums) - 1
 	for left <= right {
-		mid := (left + right) / 2
 
+		if right-left < 5 {
+			for i := left; i <= right; i++ {
+				if nums[i] == target {
+					return i
+				}
+			}
+			return -1
+		}
+
+		mid := (right + left) / 2
 		if nums[mid] == target {
 			return mid
 		}
 
-		if nums[mid] >= nums[left] {
-			if target >= nums[left] && target <= nums[mid] {
-				return binarySearch(nums, left, mid-1, target)
+		if target < nums[left] && target > nums[right] {
+			return -1
+		}
+
+		if nums[left] < nums[mid] {
+			if nums[left] <= target && target <= nums[mid] {
+				right = mid - 1
 			} else {
 				left = mid + 1
-				continue
 			}
-		}
 
-		if nums[mid] <= nums[right] {
-			if target >= nums[mid] && target <= nums[right] {
-				return binarySearch(nums, mid+1, right, target)
+		} else if nums[mid] < nums[right] {
+			if nums[mid] <= target && target <= nums[right] {
+				left = mid + 1
 			} else {
 				right = mid - 1
-				continue
 			}
 		}
-	}
 
-	return -1
-
-}
-
-func binarySearch(nums []int, left, right int, target int) int {
-
-	for left <= right {
-		mid := (left + right) / 2
-
-		if nums[mid] == target {
-			return mid
-		}
-
-		if nums[mid] < target {
-			left = mid + 1
-		}
-		if nums[mid] > target {
-			right = mid - 1
-		}
-	}
-	return -1
-}
-
-func linearSearch(nums []int, left, right int, target int) int {
-
-	for i := left; i <= right; i++ {
-		if nums[i] == target {
-			return i
-		}
 	}
 
 	return -1
